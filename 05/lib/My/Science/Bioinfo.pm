c6f9987baa6c1f89b80bf5fe7085fc2c4885b94c
Rafael Lorandi <coolparadox@gmail.com>
2013-02-25 22:57:00 -0300

    Complemento reverso de uma sequência de DNA.
    Uso de módulos Perl.

---
+++ 05/lib/My/Science/Bioinfo.pm
@@ -0,0 +1,35 @@
+package My::Science::Bioinfo;
+
+#
+# Bioinformatics module
+#
+# This library was developed as a homework exercise for LaCTAD Bioinformatics Course VI.
+# By Rafael Lorandi <coolparadox@gmail.com>.
+#
+
+use base Exporter;
+our @EXPORT_OK = ("is_dna", "dna_complement");
+our %EXPORT_TAGS = (all => \@EXPORT_OK);
+
+use strict;
+use warnings;
+
+# Does a string represent DNA?
+sub is_dna {
+
+	my ($sequence) = @_;
+	return !($sequence =~ m/[^aAcCgGtT]/);
+
+}
+
+# Reverse complement a DNA sequence.
+sub dna_complement {
+
+	my ($sequence) = @_;
+	$sequence =~ tr/aAcCgGtT/tTgGcCaA/;
+	return scalar reverse $sequence;
+
+}
+
+1;
+
